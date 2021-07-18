d8d03fd0186acec9f0f39aad6838389131f23100
Reji-ot <reji.ot@gmail.com>
2021-07-18 22:57:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -44,6 +44,18 @@ $(document).ready(function () {
     autoplaySpeed: 7000,
     autoplayHoverPause: true,
   })
+
+  // isotope filter
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+  })
+
+  // filter items on button click
+  $('.button-group').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter')
+    $grid.isotope({ filter: filterValue })
+  })
 })
 
 // login page
